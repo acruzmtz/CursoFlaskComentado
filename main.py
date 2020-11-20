f4a3a00447f54f57e8f0122c6555845dfc5441e6
acruzmtz <alejandrocruzmtz@outlook.com>
2020-11-20 06:15:14 -0600

    get user's IP

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hola mundo"
+    user_ip = request.remote_addr
+    return f"Hola mundo, tu ip es {user_ip}"
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
