771c643bf2e04bd43efd5412bbda3c86f0a9d813
acruzmtz <alejandrocruzmtz@outlook.com>
2020-11-21 06:27:26 -0600

    use function errorhandler to configure error page

--- main.py
+++ main.py
@@ -21,7 +21,21 @@ def hello():
         'user_ip': user_ip,
         'todos': todos
     }
+
+    #raise(Exception('500 Error'))
     return render_template('hello.html', **context)
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html', error=error)
+
+
+@app.errorhandler(500)
+def internal_server_error(error):
+    return render_template('500.html', error= error)
+
+
 
 
 if __name__ == '__main__':
