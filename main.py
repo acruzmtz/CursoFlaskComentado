24c09fb811ae2ab9c831d9c24b3e28de41148ea1
acruzmtz <alejandrocruzmtz@outlook.com>
2020-11-24 03:45:43 -0600

    start the testing app, with unittest and flask-testing

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms.fields import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired
-
+import unittest
 
 app = Flask(__name__)
 boostrap = Bootstrap(app)
@@ -15,15 +15,20 @@ todos = ['Mandar diseños navidad', 'Cerrar trato con proveedor', 'Entregar avan
 class LoginForm(FlaskForm):
     username = StringField('Nombre de usuario: ', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
-    confirm_password = PasswordField('Confirma Password', validators=[DataRequired()])
-
-    if password == confirm_password:
-        print('son iguales')
-    else:
-        print('no lo son')
+    # confirm_password = PasswordField('Confirma Password', validators=[DataRequired()])
+    #
+    # if password == confirm_password:
+    #     print('son iguales')
+    # else:
+    #     print('no lo son')
 
     submit = SubmitField('Enviar')
 
+
+@app.cli.command()
+def test():
+    test = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner().run(test)
 
 @app.route('/')
 def home():
@@ -67,8 +72,6 @@ def not_found(error):
 @app.errorhandler(500)
 def internal_server_error(error):
     return render_template('500.html', error= error)
-
-
 
 
 if __name__ == '__main__':
